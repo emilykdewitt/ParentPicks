0f9ba016f9fc26dcd40680706d268b83f5cb2db6
EmilyDeWitt <emilykdewitt@gmail.com>
2020-01-21 21:32:52 -0600

    Got started on authentication through Firebase

--- parentpicks.ui/src/App/App.js
+++ parentpicks.ui/src/App/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConnection from '../DataRequests/firebaseConnection';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <button className="btn btn-danger">help</button>
       </header>
     </div>
